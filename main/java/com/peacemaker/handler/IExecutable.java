f8ac9b903d6126863ae30e2019c6fc66c3718af6
Denys Solyanyk <peacemaker@ukr.net>
2015-10-05 11:29:47 +0300

    Add code

---
+++ main/java/com/peacemaker/handler/IExecutable.java
@@ -0,0 +1,11 @@
+package com.peacemaker.handler;
+
+/**
+ * @author Denys Solianyk <peacemaker@ukr.net>
+ * @since 2011-06-13
+ */
+public interface IExecutable {
+
+    boolean execute() throws Exception;
+
+}
